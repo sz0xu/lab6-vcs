1ccd5a7a64f8b42b5cee9373bce2b110d262d8bc
narutyunov <narutyunov@ufl.edu>
2024-10-17 19:09:13 -0400

    Added decode function

--- encoder.py
+++ encoder.py
@@ -8,7 +8,13 @@ def encode(string):
     return encode
 
 def decode(string):
-    pass
+    decoded_password_string = ""
+    for i in string:
+        i = int(i) - 3
+        decoded_password_string += str(i)
+
+    return decoded_password_string
+    
 
 def main():
     while True:
@@ -19,7 +25,7 @@ def main():
             hold = encode(pw)
             print("Your password has been encoded and stored!\n")
         elif op == 2:
-            print(f"The encoded password is {hold}, and the original password is {decode(hold)}") #i might have done too much formatting here. if you dont like this solution, you can edit this elif
+            print(f"The encoded password is {hold}, and the original password is {decode(hold)}.") # I think the formatting is fine the way it is. I added a period since the example output had one.
         elif op == 3:
             exit()
 
